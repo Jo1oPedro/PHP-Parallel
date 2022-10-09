d9b919b74e1d507f4fad793fc298119e8e142361
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-09 01:16:14 -0300

    adiciona retorno de threads para calcular pontos dos alunos

---
+++ calculate-points.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use parallel\Runtime;
+use parallel\Channel;
+
+$repository = new \Alura\Threads\Student\InMemoryStudentRepository();
+$studentList = $repository->all();
+
+$totalPoints = 0;
+$runTimes = [];
+$futures = [];
+$channel = Channel::make('points');
+foreach($studentList as $key => $student) {
+    $activities = $repository->activitiesInADay($student);
+
+    $runTimes[$key] = new Runtime(__DIR__ . '/vendor/autoload.php');
+
+    $futures[] = $runTimes[$key]->run(function (array $activities, \Alura\Threads\Student\Student $student, Channel $channel) {
+         $points = array_reduce(
+            $activities,
+            fn (int $total, \Alura\Threads\Activity\Activity $activity) => $total + $activity->points(),
+            0
+        );
+
+        printf('%s made %d poinst today%s', $student->fullName(), $points, PHP_EOL);
+
+        return $points;
+    }, [$activities, $student, $channel]);
+
+}
+
+$totalPoints = array_reduce($futures, fn($totalPoints, $future) => $totalPoints += $future->value(), 0) . PHP_EOL;
+
+printf('We had a total of %d points today%s', $totalPoints, PHP_EOL);
